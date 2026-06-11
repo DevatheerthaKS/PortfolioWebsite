faa279bed330f4ecbda67c67e5c495843c38ba42
DevaTheertha <ksdevatheertha@gmail.com>
2026-06-11 17:02:21 +0530

    Added new files and updates

---
+++ script.js
@@ -0,0 +1,24 @@
+const themeBtn =
+document.getElementById("theme-toggle");
+
+themeBtn.addEventListener("click", () => {
+  document.body.classList.toggle("light-mode");
+});
+
+const greeting =
+document.getElementById("greeting");
+
+const hour = new Date().getHours();
+
+if(hour < 12){
+  greeting.textContent =
+  "Good Morning";
+}
+else if(hour < 18){
+  greeting.textContent =
+  "Good Afternoon";
+}
+else{
+  greeting.textContent =
+  "Good Evening";
+}
